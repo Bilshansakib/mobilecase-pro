3ecca9cda17cd39ae6db367d805ef52c2e41dfc2
Bilshansakib <burakaghani@gmail.com>
2024-11-19 02:20:57 +0600

    Update: Icons component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Icons } from "@/components/icons";
 import MaxWithWrapper from "@/components/MaxWithWrapper";
 import Phone from "@/components/Phone";
 import { Check, Star } from "lucide-react";
@@ -100,6 +101,22 @@ export default function Home() {
             </div>
           </div>
         </MaxWithWrapper>
+      </section>
+{/* value proposition section */}
+      <section className='bg-slate-100 grainy-dark py-24'>
+     <MaxWithWrapper  className='flex flex-col items-center gap-16 sm:gap-32'>
+     <div className='flex flex-col lg:flex-row items-center gap-4 sm:gap-6'>
+            <h2 className='order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900'>
+              What our{' '}
+              <span className='relative px-2'>
+                customers{' '}
+                <Icons.Underline className='hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500' />
+              </span>{' '}
+              say
+            </h2>
+            <img src='/snake-2.png' className='w-24 order-0 lg:order-2' />
+          </div>
+      </MaxWithWrapper>   
       </section>
     </div>
   );
