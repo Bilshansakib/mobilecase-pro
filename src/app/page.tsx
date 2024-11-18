9088afea5408f2d5884d786c6e9edfcb98c210dd
Bilshansakib <burakaghani@gmail.com>
2024-11-19 02:30:57 +0600

    Update: first costomer review

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -83,40 +83,80 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            
           </div>
-          <div className='col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit'>
-            <div className='relative md:max-w-xl'>
+          <div className="col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit">
+            <div className="relative md:max-w-xl">
               <img
-                src='/your-image.png'
-                className='absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block'
-                alt='some value'
+                src="/your-image.png"
+                className="absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block"
+                alt="some value"
               />
               <img
-                src='/line.png'
-                className='absolute w-20 -left-6 -bottom-6 select-none'
-                alt='some value'
+                src="/line.png"
+                className="absolute w-20 -left-6 -bottom-6 select-none"
+                alt="some value"
               />
-              <Phone className='w-64' imageSrc='/testimonials/1.jpg' />
+              <Phone className="w-64" imageSrc="/testimonials/1.jpg" />
             </div>
           </div>
         </MaxWithWrapper>
       </section>
-{/* value proposition section */}
-      <section className='bg-slate-100 grainy-dark py-24'>
-     <MaxWithWrapper  className='flex flex-col items-center gap-16 sm:gap-32'>
-     <div className='flex flex-col lg:flex-row items-center gap-4 sm:gap-6'>
-            <h2 className='order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900'>
-              What our{' '}
-              <span className='relative px-2'>
-                customers{' '}
-                <Icons.Underline className='hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500' />
-              </span>{' '}
+      {/* value proposition section */}
+      <section className="bg-slate-100 grainy-dark py-24">
+        <MaxWithWrapper className="flex flex-col items-center gap-16 sm:gap-32">
+          <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
+            <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+              What our{" "}
+              <span className="relative px-2">
+                customers{" "}
+                <Icons.Underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500" />
+              </span>{" "}
               say
             </h2>
-            <img src='/snake-2.png' className='w-24 order-0 lg:order-2' />
+            <img src="/snake-2.png" className="w-24 order-0 lg:order-2" />
           </div>
-      </MaxWithWrapper>   
+          <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+              </div>
+              <div className="text-lg leading-8">
+                <p>
+                  "The case feels durable and I even got a compliment on the
+                  design. Had the case for two and a half months now and{" "}
+                  <span className="p-0.5 bg-slate-800 text-white">
+                    the image is super clear
+                  </span>
+                  , on the case I had before, the image started fading into
+                  yellow-ish color after a couple weeks. Love it."
+                </p>
+              </div>
+              <div className="flex gap-4 mt-2">
+                <img
+                  className="rounded-full h-12 w-12 object-cover"
+                  src="/users/user-1.png"
+                  alt="user"
+                />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Jonathan</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className="h-4 w-4 stroke-[3px] text-green-600" />
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* second user review */}
+
+          
+          
+        </MaxWithWrapper>
       </section>
     </div>
   );
