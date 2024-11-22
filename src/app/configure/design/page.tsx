beb84493c36024a8195d288df973b144d0c45ab4
Bilshansakib <burakaghani@gmail.com>
2024-11-23 03:09:36 +0600

    Update: Rnd dragging image added

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,17 +1,36 @@
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+import DesignConfigurator from "./DesignConfigurator";
+
 interface PageProps {
-    searchParams: {
-      [key: string]: string | string[] | undefined
-    }
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  // make db call
+  const { id } = searchParams;
+
+  if (!id || typeof id !== "string") {
+    return notFound();
   }
 
-const Page = ({searchParams}: PageProps) => {
-    // make db call
-    const {id } = searchParams
-    return (
-        <div>
-            {id}
-        </div>
-    );
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  });
+
+  if (!configuration) {
+    return notFound();
+  }
+  const { imageUrl, width, height } = configuration;
+  return (
+    <DesignConfigurator
+      configId={configuration.id}
+      imageDimensions={{ width, height }}
+      imageUrl={imageUrl}
+    ></DesignConfigurator>
+  );
 };
 
 export default Page;
