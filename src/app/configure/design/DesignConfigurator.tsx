b059686fac1ed00b3d84fc9da917e58db4cf1b8c
Bilshansakib <burakaghani@gmail.com>
2024-11-24 02:10:28 +0600

    Update: Material & Finish part added

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import HandleComponent from "@/components/HandleComponent";
-import { cn } from "@/lib/utils";
+import { cn, formatPrice } from "@/lib/utils";
 import { RadioGroup } from "@headlessui/react";
 import { AspectRatio } from "@radix-ui/react-aspect-ratio";
 import { ScrollArea } from "@radix-ui/react-scroll-area";
@@ -10,7 +10,7 @@ import { Rnd } from "react-rnd";
 
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
-import { COLORS, MODELS } from "@/validators/option-validator";
+import { COLORS, FINISHES, MATERIALS, MODELS } from "@/validators/option-validator";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
 import { Check, ChevronsUpDown } from "lucide-react";
 import { useState } from "react";
@@ -28,13 +28,13 @@ const DesignConfigurator = ({
   const [options, setOptions] = useState<{
     color: (typeof COLORS)[number];
     model: (typeof MODELS.options)[number]
-    // material: (typeof MATERIALS.options)[number]
-    // finish: (typeof FINISHES.options)[number]
+    material: (typeof MATERIALS.options)[number]
+    finish: (typeof FINISHES.options)[number]
   }>({
     color: COLORS[0],
     model: MODELS.options[0],
-    // material: MATERIALS.options[0],
-    // finish: FINISHES.options[0],
+    material: MATERIALS.options[0],
+    finish: FINISHES.options[0],
   });
   console.log(options)
 
@@ -178,7 +178,66 @@ const DesignConfigurator = ({
                     </DropdownMenuContent>
                   </DropdownMenu>
                 </div>
+                {[MATERIALS, FINISHES].map(
+                  ({ name, options: selectableOptions }) => (
+                    <RadioGroup
+                      key={name}
+                      value={options[name]}
+                      onChange={(val) => {
+                        setOptions((prev) => ({
+                          ...prev,
+                          [name]: val,
+                        }))
+                      }}>
+                      <Label>
+                        {name.slice(0, 1).toUpperCase() + name.slice(1)}
+                      </Label>
+                      <div className='mt-3 space-y-4'>
+                        {selectableOptions.map((option) => (
+                          <RadioGroup.Option
+                            key={option.value}
+                            value={option}
+                            className={({ active, checked }) =>
+                              cn(
+                                'relative block cursor-pointer rounded-lg bg-white px-6 py-4 shadow-sm border-2 border-zinc-200 focus:outline-none ring-0 focus:ring-0 outline-none sm:flex sm:justify-between',
+                                {
+                                  'border-primary': active || checked,
+                                }
+                              )
+                            }>
+                            <span className='flex items-center'>
+                              <span className='flex flex-col text-sm'>
+                                <RadioGroup.Label
+                                  className='font-medium text-gray-900'
+                                  as='span'>
+                                  {option.label}
+                                </RadioGroup.Label>
 
+                                {option.description ? (
+                                  <RadioGroup.Description
+                                    as='span'
+                                    className='text-gray-500'>
+                                    <span className='block sm:inline'>
+                                      {option.description}
+                                    </span>
+                                  </RadioGroup.Description>
+                                ) : null}
+                              </span>
+                            </span>
+
+                            <RadioGroup.Description
+                              as='span'
+                              className='mt-2 flex text-sm sm:ml-4 sm:mt-0 sm:flex-col sm:text-right'>
+                              <span className='font-medium text-gray-900'>
+                                {formatPrice(option.price / 100)}
+                              </span>
+                            </RadioGroup.Description>
+                          </RadioGroup.Option>
+                        ))}
+                      </div>
+                    </RadioGroup>
+                  )
+                )}
               </div>
             </div>
           </div>
