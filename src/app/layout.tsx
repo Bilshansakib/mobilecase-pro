53041a9458927fb7fbfc0ecab125b6957a24d6ff
Bilshansakib <burakaghani@gmail.com>
2024-11-27 15:55:15 +0600

    Update: payment section added using stripe

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,24 @@
 import type { Metadata } from "next";
 
-import { Recursive } from 'next/font/google'
-import localFont from "next/font/local";
-import "./globals.css";
-import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
-import { Toaster } from "@/components/ui/toaster";
+import NavBar from "@/components/NavBar";
 import Providers from "@/components/Providers";
+import { Toaster } from "@/components/ui/toaster";
+import { Recursive } from 'next/font/google';
+import "./globals.css";
 
 const recursive = Recursive({ subsets: ['latin'] })
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -37,7 +36,7 @@ export default function RootLayout({
         className={`${recursive.className} ${recursive.className} antialiased`}
       >
         <NavBar></NavBar>
-        <main className="flex flex-col min-h-[calc(100vh-3.5rem-1px)]">
+        <main className="flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]">
           <div className="flex-1 flex flex-col h-full">
             <Providers> {children}</Providers>
           </div>
